efb4b5592429c3190cf50c35d55af5125db1a9dc
Gary Marroquin <garymarroquin@Garys-MacBook-Pro.local>
2017-09-27 05:53:59 -0500

    pseudo code added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
-// This code will run as soon as the page loads
+//runs as soon as the page loads
 window.onload = function() {
-  $("#start").on("click", timer.start);
+	$("#start").on("click", timer.start);
 };
 
 var intervalId;
@@ -9,40 +9,65 @@ var clockRunning = false;
 //timer
 var timer = {
 
-  time: 0,
-  start: function() {
+	time: 0,
+	start: function() {
 
-    //sets the speed of the countdown by one second
-    if (!clockRunning) {
-        intervalId = setInterval(timer.count, 1000);
-        clockRunning = true;
-    }
-  },
-  count: function() {
-    // count down by 1
-    timer.time--;
+//sets the speed of the countdown by one second
+if (!clockRunning) {
+	intervalId = setInterval(timer.count, 1000);
+	clockRunning = true;
+}
+},
 
-    var converted = timer.timeConverter(timer.time);
-    console.log(converted);
+count: function() {
+// count down by 1
+timer.time--;
 
-    //displays time in the footer of main section timeLeft
-    $("#timeLeft").html(converted);
-  },
-  timeConverter: function(t) {
+var converted = timer.timeConverter(timer.time);
+console.log(converted);
 
-    var minutes = Math.floor(t / 60);
-    var seconds = t - (minutes * 60);
+//displays time in the footer of main section timeLeft
+$("#timeLeft").html(converted);
+},
+timeConverter: function(t) {
 
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-    }
+	var minutes = Math.floor(t / 60);
+	var seconds = t - (minutes * 60);
 
-    if (minutes === 10) {
-      minutes = "00";
-    }
-    else if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-    return minutes + ":" + seconds;
-  }
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	if (minutes === 10) {
+		minutes = "00";
+	}
+	else if (minutes < 10) {
+		minutes = "00" + minutes;
+	}
+	return minutes + ":" + seconds;
+}
+
 };
+
+//If Yes button is pressed 
+	$("#answerOne").on("click", function() {
+		alert("Doing it right!")
+});
+
+//then add points in favor
+
+//continue to next question 
+
+//If No button is pressed 
+
+//then add points to losses 
+
+//continue to next question 
+
+
+
+
+
+
+
+
+
